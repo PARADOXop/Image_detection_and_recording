643227851642063b31db1fcafbd2fe45f3f87124
PARADOXop <PARADOXop@gmail.com>
2020-08-07 19:32:03 +0530

    not a msg

--- stud_info.py
+++ stud_info.py
@@ -1,39 +1,55 @@
-import array
-import numpy
-n = int(input('enter the number of students in school:-'))
-A =numpy.zeros(n)
-for e in A:
-    print(e)
 
-for i in range(1, n+1):
-    print(i)
-class school :
-    name = 'MODEL ENGLISH SCHOOL'
-
-    def get_info(self):
-        self.name = input('enter name of the student:')
-        self.rollno = int(input('enter rollno of the student'))
-        self.age = int(input('enter the age'))
-    def show_info(self):
-        print('***************************************************************')
-        print(self.name)
-        print(self.rollno)
-        print(self.age)
-        print('***************************************************************')
-
-    @classmethod
-    def school(cls):
-        print(school.name)
+try:
+    n = int(input('enter the number of students in school:-'))
+    if n == 0:
+        print('stduent in the school can\'t be zero')
+        raise ValueError()
+except ValueError :
+    if n == 0:
+        print('how the hell are you')
+except:
+    print('something went wrong')
+else:
+    A = list(range(1, n + 1))
 
 
+    class school:
+        name = 'MODEL ENGLISH SCHOOL'
 
-for i in range(1, n+1):
-  S = 'S' + str(i)
-  print('for student',S)
-  S = school()
-  S.get_info()
-  for j in range(1, n+1):
-        E = 'E' + str(j)
-        E = S
-        E.show_info()
-print(school.name)
+        def get_info(self):
+            self.name = input('enter name of the student:')
+            self.rollno = int(input('enter rollno of the student'))
+            self.age = int(input('enter the age'))
+
+        def show_info(self):
+            print('***************************************************************')
+            print('name of the student :', self.name)
+            print('RollNo. of the student is :', self.rollno)
+            print('RollNo. of the student is :', self.age)
+            print('***************************************************************')
+
+        @classmethod
+        def school(cls):
+            print(school.name)
+
+
+    print('***************************', school.name, '*******************************')
+
+    for i in range(1, n + 1):
+        S = 'S' + str(i)
+        print('for student', S)
+        S = school()
+        S.get_info()
+        A.insert(i, S)
+    print('do you want to print info about student:')
+    print('1 for yess and 2 for no')
+    x =int(input('enter the value'))
+    if x == 1:
+        for j in range(1, n + 1):
+            A[j].show_info()
+    else:
+        print('go back home kid what are you doi\'n here')
+    print('*******************************', school.name, '******************************')
+
+finally :
+    print('we are equally fucked up')
